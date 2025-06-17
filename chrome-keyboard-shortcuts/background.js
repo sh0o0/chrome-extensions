b2ef2e66e061b16e9a93b45d2e1efbeaa5f6221f
Shotaro Nakata <52538706+sh0o0@users.noreply.github.com>
2025-06-17 10:52:39 +0900

    add button to copy

--- chrome-keyboard-shortcuts/background.js
+++ chrome-keyboard-shortcuts/background.js
@@ -1,9 +1,17 @@
 chrome.commands.onCommand.addListener((command) => {
   if (command === "copy-page-link") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs[0]) {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "copyPageLink" });
-      }
-    });
+    copyPageLink();
   }
 });
+
+chrome.action.onClicked.addListener((tab) => {
+  copyPageLink();
+});
+
+function copyPageLink() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "copyPageLink" });
+    }
+  });
+}
